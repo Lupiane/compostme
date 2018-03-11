b6168745490c9884468333e98bd29118dffd1386
Pauline Gaget <lupiane@hotmail.com>
2018-03-11 23:09:31 +0100

    added type to compost model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180311214905) do
+ActiveRecord::Schema.define(version: 20180311220226) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20180311214905) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "type"
     t.index ["user_id"], name: "index_composts_on_user_id"
   end
 
