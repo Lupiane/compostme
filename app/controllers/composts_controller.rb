2af8c08f3d2386e9c01b28a33f907efd968f84fa
Pauline Gaget <lupiane@hotmail.com>
2018-03-11 23:14:20 +0100

    very basic CRUD for composts

--- app/controllers/composts_controller.rb
+++ app/controllers/composts_controller.rb
@@ -2,20 +2,38 @@ class CompostsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
+    @composts = Compost.all
   end
 
   def show
+    @compost = Compost.find(params[:id])
   end
 
   def new
+    @compost = Compost.new
   end
 
   def create
+    @compost = Compost.new(compost_params)
+    @compost.user = current_user
+    if @compost.save
+      redirect_to compost_path(@compost)
+    else
+      render :new
+    end
   end
 
   def edit
+    @compost = Compost.find(params[:id])
   end
 
   def update
+    @compost = Compost.find(params[:id])
+    @compost.update(compost_params)
+    if @compost.save
+      redirect_to compost_path(@compost)
+    else
+      render :edit
+    end
   end
 end
