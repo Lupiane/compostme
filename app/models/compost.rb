846a2f305111e00bf7d441af1959bbe87c2ca2a3
Pauline Gaget <lupiane@hotmail.com>
2018-03-21 13:47:01 +0100

    addition validation on compost type

--- app/models/compost.rb
+++ app/models/compost.rb
@@ -1,6 +1,6 @@
 class Compost < ApplicationRecord
   belongs_to :user
   validates :address, presence: :true
-  validates :type, presence: :true
+  validates :type, presence: :true, inclusion: in: ["Public", "Private"]
   validates :description, presence: :true
 end
