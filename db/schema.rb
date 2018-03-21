93a69497d294dd01898dec58a9efdd57d84fa0cc
Pauline Gaget <lupiane@hotmail.com>
2018-03-21 15:53:56 +0100

    geocoding works - gmaps for the moment

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180321135339) do
+ActiveRecord::Schema.define(version: 20180321141324) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(version: 20180321135339) do
     t.datetime "updated_at", null: false
     t.boolean "public", default: false
     t.boolean "deleted", default: false
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_composts_on_user_id"
   end
 
