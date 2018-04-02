afb61abe0c955948629507cb9d1b16e6fcd3569d
Pauline Gaget <lupiane@hotmail.com>
2018-04-02 16:44:18 +0200

    very basic message board service in place

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
   has_many :composts
   has_many :conversations
+  has_many :messages
   validates :username, presence: true, uniqueness: true
 
   mount_uploader :photo, PhotoUploader
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,12 @@ Rails.application.routes.draw do
     member do
       get 'remove', to: 'composts#remove'
     end
+
+    resources :messages, only: :create
+  end
+
+  resources :conversations, only: [] do
+    resources :messages, only: :index
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
