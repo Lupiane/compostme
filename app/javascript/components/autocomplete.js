f79c4de0297f9df09ce48bcd92b0d59e9bdab200
Pauline Gaget <lupiane@hotmail.com>
2018-04-02 18:20:38 +0200

    in progress still

--- app/javascript/components/autocomplete.js
+++ app/javascript/components/autocomplete.js
@@ -11,20 +11,26 @@ function autocomplete() {
       });
     }
   });
-  // var modal = document.querySelector("#modal")
-  // modal.addEventListener("", function() {
-  //   console.log("Hello JS!")
-  //   var compostAddress = document.getElementById('compost_address');
 
-  //   if (compostAddress) {
-  //     var autocomplete = new google.maps.places.Autocomplete(compostAddress, { types: [ 'geocode' ] });
-  //     google.maps.event.addDomListener(compostAddress, 'keydown', function(e) {
+// IN PROGRESS: make JS work on autocomplete inide iteration of all composts in admin dashboard
+  // document.addEventListener("DOMContentLoaded", function() {
+  //   var address = document.querySelectorAll('.data-address');
+
+  //  here address is a NodeList : JS to be applied to each element of the list....
+
+  //   if (address) {
+  //     var autocomplete = new google.maps.places.Autocomplete(address, { types: [ 'geocode' ] });
+  //     google.maps.event.addDomListener(address, 'keydown', function(e) {
   //       if (e.key === "Enter") {
   //         e.preventDefault(); // Do not submit the form on Enter.
   //       }
   //     });
   //   }
   // });
+
+
+// IN PROGRESS: make JS work for form loading inside modals
+//  .....
 }
 
 export { autocomplete };
