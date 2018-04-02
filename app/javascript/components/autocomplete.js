c0b6c0a35de7cededfa98f0b4f3d00488bb6c3d3
Pauline Gaget <lupiane@hotmail.com>
2018-04-02 17:31:11 +0200

    in progress

--- app/javascript/components/autocomplete.js
+++ app/javascript/components/autocomplete.js
@@ -11,6 +11,20 @@ function autocomplete() {
       });
     }
   });
+  // var modal = document.querySelector("#modal")
+  // modal.addEventListener("", function() {
+  //   console.log("Hello JS!")
+  //   var compostAddress = document.getElementById('compost_address');
+
+  //   if (compostAddress) {
+  //     var autocomplete = new google.maps.places.Autocomplete(compostAddress, { types: [ 'geocode' ] });
+  //     google.maps.event.addDomListener(compostAddress, 'keydown', function(e) {
+  //       if (e.key === "Enter") {
+  //         e.preventDefault(); // Do not submit the form on Enter.
+  //       }
+  //     });
+  //   }
+  // });
 }
 
 export { autocomplete };
