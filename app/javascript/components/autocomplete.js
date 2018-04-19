df3863dee8583840701aa38adb767e1c4e934edf
Pauline Gaget <lupiane@hotmail.com>
2018-04-19 11:15:30 +0200

    fixed address autocomplete issue in admin dashboard

--- app/javascript/components/autocomplete.js
+++ app/javascript/components/autocomplete.js
@@ -1,32 +1,20 @@
 function autocomplete() {
   document.addEventListener("DOMContentLoaded", function() {
-    var compostAddress = document.getElementById('compost_address');
-
-    if (compostAddress) {
-      var autocomplete = new google.maps.places.Autocomplete(compostAddress, { types: [ 'geocode' ] });
-      google.maps.event.addDomListener(compostAddress, 'keydown', function(e) {
-        if (e.key === "Enter") {
-          e.preventDefault(); // Do not submit the form on Enter.
+    document.querySelectorAll('.compost_address').forEach((addressField) => {
+      addressField.addEventListener("focus", (e) => {
+        var compostAddress = e.currentTarget;
+        if (compostAddress) {
+          var autocomplete = new google.maps.places.Autocomplete(compostAddress, { types: [ 'geocode' ] });
+          google.maps.event.addDomListener(compostAddress, 'keydown', function(e) {
+            if (e.key === "Enter") {
+              e.preventDefault(); // Do not submit the form on Enter.
+            }
+          });
         }
       });
-    }
+    });
   });
 
-// IN PROGRESS: make JS work on autocomplete inide iteration of all composts in admin dashboard
-  // document.addEventListener("DOMContentLoaded", function() {
-  //   var address = document.querySelectorAll('.data-address');
-
-  //  here address is a NodeList : JS to be applied to each element of the list....
-
-  //   if (address) {
-  //     var autocomplete = new google.maps.places.Autocomplete(address, { types: [ 'geocode' ] });
-  //     google.maps.event.addDomListener(address, 'keydown', function(e) {
-  //       if (e.key === "Enter") {
-  //         e.preventDefault(); // Do not submit the form on Enter.
-  //       }
-  //     });
-  //   }
-  // });
 
 
 // IN PROGRESS: make JS work for form loading inside modals
