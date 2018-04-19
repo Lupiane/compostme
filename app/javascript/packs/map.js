8390dda6f6e94ff455cf11cd307ec0dd5422f5ea
Pauline Gaget <lupiane@hotmail.com>
2018-04-19 15:34:53 +0200

    last change forgotten before last push

--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -239,8 +239,8 @@ const mapElement = document.getElementById('map');
 if (mapElement) { // don't try to build a map if there's no div#map to inject in
   const map = new GMaps({ el: '#map', lat: 0, lng: 0 });
   const markers = JSON.parse(mapElement.dataset.markers);
-  const pubmrkrs = JSON.parse(mapElement.dataset.pubmrkrs);
   map.addMarkers(markers);
+  const pubmrkrs = JSON.parse(mapElement.dataset.pubmrkrs);
   map.addMarkers(pubmrkrs);
   if (markers.length === 0) {
     map.setZoom(2);
