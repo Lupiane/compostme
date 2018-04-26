e61c65d74de83f0dbb8136faa70668ef37d952e2
Pauline Gaget <lupiane@hotmail.com>
2018-04-26 15:20:52 +0200

    styling show page and made adjustments to map js code

--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -238,18 +238,20 @@ const styles = [
 const mapElement = document.getElementById('map');
 if (mapElement) { // don't try to build a map if there's no div#map to inject in
   const map = new GMaps({ el: '#map', lat: 0, lng: 0 });
-  const markers = JSON.parse(mapElement.dataset.markers);
+  var markers = JSON.parse(mapElement.dataset.markers);
   map.addMarkers(markers);
-  const pubmrkrs = JSON.parse(mapElement.dataset.pubmrkrs);
-  map.addMarkers(pubmrkrs);
-  const allMarkers = markers.concat(pubmrkrs)
-  if (allMarkers.length === 0) {
+  if (mapElement.dataset.pubmrkrs != undefined) {
+    const pubmrkrs = JSON.parse(mapElement.dataset.pubmrkrs);
+    map.addMarkers(pubmrkrs);
+    markers = markers.concat(pubmrkrs);
+  }
+  if (markers.length === 0) {
     map.setZoom(2);
-  } else if (allMarkers.length === 1) {
-    map.setCenter(allMarkers[0].lat, allMarkers[0].lng);
+  } else if (markers.length === 1) {
+    map.setCenter(markers[0].lat, markers[0].lng);
     map.setZoom(14);
   } else {
-    map.fitLatLngBounds(allMarkers);
+    map.fitLatLngBounds(markers);
   }
   map.addStyle({
     styles: styles,
