02889ba2e3166570736935a06927d1ca94a19c01
Pauline Gaget <lupiane@hotmail.com>
2018-06-04 13:32:57 +0200

    autocomplete works but needs to replace address fields in all forms. In progress

--- app/javascript/components/autocomplete.js
+++ app/javascript/components/autocomplete.js
@@ -1,15 +1,52 @@
+import mapboxgl from 'mapbox-gl';
+import MapboxGeocoder from 'mapbox-gl-geocoder';
+
 function autocomplete() {
   document.addEventListener("DOMContentLoaded", function() {
-    document.querySelectorAll('.compost_address').forEach((addressField) => {
+    var addresses = document.querySelectorAll('.compost_address')
+    if (addresses.length >= 1) {
+      mapboxgl.accessToken = 'pk.eyJ1IjoibHVwaWFuZSIsImEiOiJjamhobnlhNDgyM3loMzBzNjdlNjd0cDd4In0.Bi6PxCUwLgCJTlmZ6zjVEA';
+
+      var map = new mapboxgl.Map({
+        container: 'fakemap',
+        style: 'mapbox://styles/mapbox/streets-v9',
+        center: [-79.4512, 43.6568],
+        zoom: 13
+      });
+
+
+      var geocoder = new MapboxGeocoder({
+        accessToken: mapboxgl.accessToken,
+        autocomplete: true
+      });
+    };
+
+    addresses.forEach((addressField) => {
       addressField.addEventListener("focus", (e) => {
         var compostAddress = e.currentTarget;
         if (compostAddress) {
-          var autocomplete = new google.maps.places.Autocomplete(compostAddress, { types: [ 'geocode' ] });
-          google.maps.event.addDomListener(compostAddress, 'keydown', function(e) {
-            if (e.key === "Enter") {
-              e.preventDefault(); // Do not submit the form on Enter.
-            }
-          });
+          // var autocomplete = new google.maps.places.Autocomplete(compostAddress, { types: [ 'geocode' ] });
+          // google.maps.event.addDomListener(compostAddress, 'keydown', function(e) {
+          //   if (e.key === "Enter") {
+          //     e.preventDefault(); // Do not submit the form on Enter.
+          //   }
+          // });
+
+          document.getElementById('geocoder').appendChild(geocoder.onAdd(map));
+          document.querySelector(".mapboxgl-ctrl-geocoder > input").addEventListener("change", function(e) {
+            console.log(geocoder._typeahead.selected.place_name);
+            compostAddress.value = geocoder._typeahead.selected.place_name;
+          })
+
+          // query the geocoder
+
+          // add event listener so query updates with user typing
+
+          // geocoder.addDomListener(compostAddress, 'keydown', function(e) {
+          //   if (e.key === "Enter") {
+          //     e.preventDefault(); // Do not submit the form on Enter.
+          //   }
+          // });
         }
       });
     });
@@ -18,3 +55,12 @@ function autocomplete() {
 
 export { autocomplete };
 
+// <script>
+// mapboxgl.accessToken = 'pk.eyJ1IjoibHVwaWFuZSIsImEiOiJjamhobnlhNDgyM3loMzBzNjdlNjd0cDd4In0.Bi6PxCUwLgCJTlmZ6zjVEA';
+
+// var geocoder = new MapboxGeocoder({
+//     accessToken: mapboxgl.accessToken
+// });
+
+// document.getElementById('geocoder').appendChild(geocoder.onAdd(map));
+// </script>
